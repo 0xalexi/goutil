fcfd0b246952cc49637d6c198f19794886efae2d
Alexi Robbins <alexi@tamber.com>
2021-09-15 17:59:48 -0700

    adds WaitFor method

---
+++ wait.go
@@ -0,0 +1,30 @@
+package goutil
+
+import (
+	"errors"
+	"time"
+)
+
+func WaitFor(f func() bool, period, timeout time.Duration) error {
+	if period < 1 {
+		period = time.Duration(2 * time.Second)
+	}
+	if timeout < 1 {
+		timeout = time.Duration(60 * time.Second)
+	}
+	t := time.Duration(0)
+	for t < timeout || timeout == NO_TIMEOUT {
+		s := time.Now()
+		if f() {
+			return nil
+		}
+		d := time.Now().Sub(s)
+		if d < period {
+			time.Sleep(period - d)
+			t += period
+		} else {
+			t += d
+		}
+	}
+	return errors.New("Timed out after " + timeout.String())
+}
